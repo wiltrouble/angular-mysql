126b922698d8048c3ccf87b4ddc99ddf87cf26bb
wiltrouble <wiltrouble@gmail.com>
2019-03-26 17:02:35 -0400

    create routes

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -3,10 +3,36 @@ import {Request, Response} from 'express';
 import pool from '../database';
 class GamesController {
 
-    public index (req: Request, res: Response) {
-        pool.query('DESCRIBE games');
-        res.json('games');
-    } 
+    public async list (req: Request, res: Response) {
+        const games = await pool.query('SELECT * FROM games');
+        res.json(games);
+    }
+
+    public async getOne(req: Request, res: Response): Promise<any> {
+        const { id } = req.params;
+        const games = await pool.query("SELECT * FROM games WHERE id = ?", [id]);
+        if (games.length > 0) {
+            return res.json(games[0]);
+        }
+        res.status(404).json({text: "the game doesnt exist"});
+    }
+    
+    public async create(req: Request, res: Response): Promise<void> {
+        await pool.query('INSERT INTO games set ?', [req.body]);
+        res.json({message: 'game saved!'});
+    }
+
+    public async delete(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        await pool.query('DELETE FROM games WHERE id = ?', [id]);
+        res.json({message: "The game was deleted"});
+    }
+
+    public async update(req: Request, res: Response): Promise<void> {
+        const { id }  = req.params;
+        await pool.query('UPDATE games set ? WHERE id = ?', [req.body, id]);
+        res.json({message: "the game was updated"});
+    }
 }
 
 const gamesController = new GamesController();

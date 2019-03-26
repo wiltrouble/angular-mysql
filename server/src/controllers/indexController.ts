2c4cfb4a240eb897eea5847953e67f0b5f3e4acb
wiltrouble <wiltrouble@gmail.com>
2019-03-26 14:15:54 -0400

    database connection

---
+++ server/src/controllers/indexController.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from 'express';
+
+class IndexController {
+
+    public index (req: Request, res: Response) {
+        res.send("hello");
+    } 
+}
+
+export const indexController = new IndexController();

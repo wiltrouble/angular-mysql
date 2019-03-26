7602b6453edd50a65beaf8721a4913486e7297a3
wiltrouble <wiltrouble@gmail.com>
2019-03-26 13:37:35 -0400

    add indexRoutes, gamesRoutes

--- server/build/index.js
+++ server/build/index.js
@@ -4,6 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
+const gamesRoutes_1 = __importDefault(require("./routes/gamesRoutes"));
 class Server {
     constructor() {
         this.app = express_1.default();
@@ -12,8 +16,13 @@ class Server {
     }
     config() {
         this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default());
+        this.app.use(express_1.default.json());
     }
     routes() {
+        this.app.use('/', indexRoutes_1.default);
+        this.app.use('/api/games', gamesRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,9 @@
 import express, {Application}from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import indexRoutes from './routes/indexRoutes';
+import gamesRoutes from './routes/gamesRoutes';
 
 class Server{
     public app: Application;
@@ -10,10 +15,14 @@ class Server{
 
     config():void {
         this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan('dev'))
+        this.app.use(cors());
+        this.app.use(express.json());
     }
 
     routes(): void {
-
+        this.app.use('/', indexRoutes);
+        this.app.use('/api/games', gamesRoutes);
     }
 
     start(): void {

a43c9f038013a3b1a7d9868c8a05e4e6784fbf4d
wiltrouble <wiltrouble@gmail.com>
2019-03-26 15:16:12 -0400

    remove build

--- server/build/controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexController {
-    index(req, res) {
-        res.send("hello");
-    }
-}
-exports.indexController = new IndexController();

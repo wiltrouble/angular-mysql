f33ea7dc9b1a64ea49a3bde7a348ff50389389b7
wiltrouble <wiltrouble@gmail.com>
2019-03-26 12:53:03 -0400

    Server on port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("Staring app");
+import express, {Application}from 'express';
+
+class Server{
+    public app: Application;
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    config():void {
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes(): void {
+
+    }
+
+    start(): void {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('Server on port', this.app.get('port'));
+        });
+    }
+}
+
+const server = new Server();
+server.start();
